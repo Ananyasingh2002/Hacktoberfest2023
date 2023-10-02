907ef79c27943e276148f562eb649658ef13d072
RudraNarayanHota-001 <89252181+Rudrahota-1@users.noreply.github.com>
2023-10-02 15:31:08 +0530

    Update Queue.c

--- All_About_DSA/DataStructuresAndAlgortihms/Queue.c
+++ All_About_DSA/DataStructuresAndAlgortihms/Queue.c
@@ -1,105 +1,98 @@
+// C program for array implementation of queue
+#include <limits.h>
 #include <stdio.h>
-
-int enqueue(int a[],int size,int element){
-    a[size]=element;
-    return 0;
+#include <stdlib.h>
+ 
+// A structure to represent a queue
+struct Queue {
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
+ 
+// function to create a queue
+// of given capacity.
+// It initializes size of queue as 0
+struct Queue* createQueue(unsigned capacity)
+{
+    struct Queue* queue
+        = (struct Queue*)malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+ 
+    // This is important, see the enqueue
+    queue->rear = capacity - 1;
+    queue->array
+        = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
 }
-int dequeue(int a[],int size){
-    printf("%d is popped\n",a[0]);
-    for (int i=0;i<size;i++){
-        a[i] = a[i+1];
-    }
-    return 0;
+ 
+// Queue is full when size becomes
+// equal to the capacity
+int isFull(struct Queue* queue)
+{
+    return (queue->size == queue->capacity);
 }
-int display(int a[],int size){
-    for (int i=0;i<size;i++){
-        printf("%d ",a[i]);
-    }
-    printf("\n");
-    return 0;
+ 
+// Queue is empty when size is 0
+int isEmpty(struct Queue* queue)
+{
+    return (queue->size == 0);
 }
-int displayFront(int a[]){
-    printf("%d\n",a[0]);
-    return 0;
+ 
+// Function to add an item to the queue.
+// It changes rear and size
+void enqueue(struct Queue* queue, int item)
+{
+    if (isFull(queue))
+        return;
+    queue->rear = (queue->rear + 1) % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+    printf("%d enqueued to queue\n", item);
 }
-int displayRear(int a[],int size){
-    printf("%d\n",a[size-1]);
-    return 0;
+ 
+// Function to remove an item from queue.
+// It changes front and size
+int dequeue(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1) % queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
 }
-int main(){
-    int n;
-    printf("Enter the length of the queue : ");
-    scanf("%d",&n);
-    if (n>0){
-        int a[n];
-        printf("-------MENU-------\n");
-        printf("-------Queue-------\n");
-        printf("1. Enqueue\n");
-        printf("2. Dequeue\n");
-        printf("3. Display\n");
-        printf("4. Display Front\n");
-        printf("5. Display Rear\n");
-        printf("6. Exit\n");
-
-        int element,size=0,choice = 0;
-        while (choice!=6){
-            printf("----Enter Command----");
-            scanf("%d",&choice);
-            switch(choice){
-                case 1:
-                if (size<n){
-                    printf("Enter the element :");
-                    scanf("%d",&element);
-                    enqueue(a,size,element);
-                    size = size+1;
-                }
-                else{
-                    printf("Full Queue\n");
-                }
-                break;
-                case 2:
-                if (size==0){
-                    printf("EMPTY QUEUE!\n");
-                }
-                else{
-                    dequeue(a,size);
-                    size = size-1;
-                }
-                break;
-                case 3:
-                if (size==0){
-                    printf("EMPTY QUEUE\n");
-                } 
-                else{
-                    display(a,size);
-                }
-                break;
-                case 4:
-                if (size==0){
-                    printf("EMPTY QUEUE\n");
-                }
-                else{
-                    displayFront(a);
-                }
-                break;
-                case 5:
-                if (size==0){
-                    printf("EMPTY QUEUE\n");
-                }
-                else{
-                    displayRear(a,size);
-                }
-                break;
-                case 6:
-                printf("-------Exiting-------\n");
-                break;
-                default:
-                printf("PLease enter a valid input !\n");
-            }
-        }
-    }
-    else{
-        printf("Length cannot be negative !\n");
-    }
+ 
+// Function to get front of queue
+int front(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->front];
+}
+ 
+// Function to get rear of queue
+int rear(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->rear];
+}
+ 
+// Driver program to test above functions./
+int main()
+{
+    struct Queue* queue = createQueue(1000);
+ 
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+    enqueue(queue, 40);
+ 
+    printf("%d dequeued from queue\n", dequeue(queue));
+    printf("Front item is %d\n", front(queue));
+    printf("Rear item is %d\n", rear(queue));
+ 
     return 0;
 }
