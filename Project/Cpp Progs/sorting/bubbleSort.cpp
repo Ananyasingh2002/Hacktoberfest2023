a89fbd1057f5be8e589c80650784f5b0b4a9428b
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2023-10-05 22:38:01 +0530

    Update bubbleSort.cpp

--- Project/Cpp Progs/sorting/bubbleSort.cpp
+++ Project/Cpp Progs/sorting/bubbleSort.cpp
@@ -1,25 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
+void bubblesort(int arr[], int n)
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        bool swaped = false;
+        for (int j = 0; j < n-i-1; j++)
         {
             if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
+                swaped = true;
+            }
+           
         }
+
+         if (swaped == false)
+            {
+                break;
+            }
+    }
+}
+int main()
+{
+    int arr[] = {2, 33, 12, 10, 34214, 7, 12};
+    bubblesort(arr, sizeof(arr) / sizeof(arr[0]));
+    for (auto i : arr)
+    {
+        cout << i << " ";
     }
 
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
     return 0;
 }
