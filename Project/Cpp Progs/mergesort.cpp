32013e12e67ac27e10ecf69a363fd880f90880d8
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2023-10-05 22:35:30 +0530

    Update mergesort.cpp

--- Project/Cpp Progs/mergesort.cpp
+++ Project/Cpp Progs/mergesort.cpp
@@ -1,66 +1,58 @@
-#include<iostream>//Preprocessig Directives
-using namespace std;//calling objects from predefined namespace
-void merge(int [],int,int,int);//declaratin of function
-void mergesort(int [],int,int);//declaration of recursive mergesort function
-void display(int [],int);           //declaration of display function
+#include<iostream>
+#include<vector>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-    int n;
-    cout<<"Enter the size of the array"<<endl;
-    cin>>n;
-    int A[n];   //declaring array of size n
-    cout<<"Enter the no of elements"<<endl;
-    for(int i=0;i<n;i++)
-        cin>>A[i];   //taking elements as input
-    mergesort(A,0,n-1);
-    display(A,n);
 
-}
-// merge function
-void merge(int A[],int low,int mid,int high)
-{
-    int n1=mid-low+1,n2=high-mid;
-    int* left = new int[n1];       //left half array pointer
-    int* right = new int[n2];      //right half array pointer
-    
-    for(int i=0;i<n1;i++)
-     left[i]=A[low+i];     //adding elements in first half of the array
-    for(int j=0;j<n2;j++)
-      right[j]=A[mid+j+1];   //adding elements in second half of the array
-    
-    int i=0,j=0,k=low;
-    while(i<n1&&j<n2)       //Code to Merge to array in sorted manner
-    {
-        if(left[i]<=right[j])
-            A[k++]=left[i++];
-        else
-            A[k++]=right[j++];
+void merge(int arr[],int l,int h,int m){
+    int n1= m-l+1;
+    int n2 = h-m;
+
+int left[n1];
+int right[n2];
+    for(int i = 0;i<n1;i++){
+        left[i] = arr[i+l];
     }
-    while(i<n1)
-        A[k++]=left[i++];       //Processing elements remaining in left array
-    while(j<n2)
-        A[k++]=right[j++];      //Processing elements remaining in right array
-
-    delete[] left;              //freeing memory after use
-    delete[] right;
-}
-//Mergesort function
-void mergesort(int arr[],int l,int h)
-{
-    if(h>l)
-    {
-        int m=(l+h)/2;  //calculating the middle index
-        mergesort(arr,l,m);
-        mergesort(arr,m+1,h);
-        merge(arr,l,m,h);
+    for(int i = 0;i< n2;i++){
+        right[i] = arr[m+i+1];
+    }
+    int k=l, i = 0,j=0;
+    while(i<n1 && j<n2){
+        if(left[i]<=right[j]){
+            arr[k++] = left[i++];
+        }
+        else{
+            arr[k++] = right[j++];
+        }
+    }
+    while(i<n1){
+        arr[k++] = left[i++];
+    }
+    while(j<n2){
+        arr[k++] = right[j++];
     }
 }
-//display function
-void display(int arr[],int n)
+
+void sort(int arr[], int l, int n)
 {
-    for(int i=0;i<n;i++)
-      cout<<arr[i]<<" ";
-    cout<<"\n";
+    if (l < n)
+    {
+        int mid = l+(n - l) / 2;
+        sort(arr, 0, mid );
+        sort(arr, mid + 1, n);
+        merge(arr, l, n, mid);
+    }
 }
 
+int main(){
+    int arr[] = {2, 33, 12, 10, 34214, 7, 12};
+    int h= (sizeof(arr) / sizeof(arr[0]));
+    sort(arr, 0, h-1);
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+   
+return 0 ;
+}
