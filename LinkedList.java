4ea83d478154e579aa0f6f1d90c00fe780089da7
Vikashchaurasia01 <91377089+Vikashchaurasia01@users.noreply.github.com>
2023-10-14 17:45:37 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,34 @@
+// Java Program to Demonstrate 
+// Implementation of LinkedList 
+// class 
+
+// Importing required classes 
+import java.util.*; 
+
+// Main class 
+public class GFG { 
+
+	// Driver code 
+	public static void main(String args[]) 
+	{ 
+		// Creating object of the 
+		// class linked list 
+		LinkedList<String> ll = new LinkedList<String>(); 
+
+		// Adding elements to the linked list 
+		ll.add("A"); 
+		ll.add("B"); 
+		ll.addLast("C"); 
+		ll.addFirst("D"); 
+		ll.add(2, "E"); 
+
+		System.out.println(ll); 
+
+		ll.remove("B"); 
+		ll.remove(3); 
+		ll.removeFirst(); 
+		ll.removeLast(); 
+
+		System.out.println(ll); 
+	} 
+}
